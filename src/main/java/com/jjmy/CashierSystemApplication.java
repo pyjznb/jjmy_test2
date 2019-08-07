495b14d984b8e59e41585010aacf7418b624f2eb
jjmy <2734704529@qq.com>
2019-08-07 12:38:59 +0800

    第一次提交：测试1

---
+++ src/main/java/com/jjmy/CashierSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jjmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashierSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashierSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jjmy/CashierSystemApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jjmy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CashierSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
